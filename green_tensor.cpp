d2eaceaad1b6496266f0aae8ac0687d2ee02a67f
Alexey Geraskin <alexey.geraskin@gmail.com>
2015-07-29 17:56:37 +0200

    population sorks, but did not tested for correctnesd

--- green_tensor.cpp
+++ green_tensor.cpp
@@ -17,9 +17,14 @@ namespace Native
 {
 	extern "C"
 	{
-		DllExport void Zaxpy(const long n, MKL_Complex16 alpha, const void *x, void *y, const MKL_INT incy)
+		DllExport void Zaxpy(const long n, MKL_Complex16 alpha, const void *x, void *y)
 		{
-			cblas_zaxpy(n, &alpha, x, 1, y, incy);
+			cblas_zaxpy(n, &alpha, x, 1, y, 1);
+		}
+
+		DllExport void Zcopy(const long n, const void *x, void *y)
+		{
+			cblas_zcopy(n, x, 1, y, 1);
 		}
 	}
 }
