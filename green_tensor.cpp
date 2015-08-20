dbfa200c0ad6c0a636058b827664a28b56c22a9d
Alexey Geraskin <alexey.geraskin@gmail.com>
2015-08-20 16:33:30 +0200

    new green tensor has dixtionary

--- green_tensor.cpp
+++ green_tensor.cpp
@@ -17,9 +17,9 @@ namespace Native
 {
 	extern "C"
 	{
-		DllExport void Zaxpy(const long n, MKL_Complex16 alpha, const void *x, void *y)
+		DllExport void Zaxpy(const long n, MKL_Complex16 alpha, const void *x, int incX, void *y, int incY)
 		{
-			cblas_zaxpy(n, &alpha, x, 1, y, 1);
+			cblas_zaxpy(n, &alpha, x, incX, y, incY);
 		}
 
 		DllExport void Zcopy(const long n, const void *x, void *y)
