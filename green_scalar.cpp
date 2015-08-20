926dd951333179144622f477e93573e5d3b70fcd
Alexey Geraskin <alexey.geraskin@gmail.com>
2015-08-20 13:51:37 +0200

    move to 2015 and c#6

--- green_scalar.cpp
+++ green_scalar.cpp
@@ -45,62 +45,13 @@ namespace Native
 				eta[i] = sqrt(lambdas[i] * lambdas[i] - value);
 		}
 
-		DllExport void CalcMultDivPlus(int length, complex<double>* a1, complex<double>* a2, complex<double>* a3, complex<double>* result)
-		{
-#pragma simd
-			for (int i = 0; i < length; i++)
-				result[i] += (a1[i] * a2[i]) / a3[i];
-		}
-
-		DllExport void CalcMultDivMinus(int length, complex<double>* a1, complex<double>* a2, complex<double>* a3, complex<double>* result)
-		{
-#pragma simd
-			for (int i = 0; i < length; i++)
-				result[i] -= (a1[i] * a2[i]) / a3[i];
-		}
-
-		DllExport void CalcDivMultPlus(int length, complex<double>* a1, complex<double>* a2, complex<double>* a3, complex<double>* result)
-		{
-#pragma simd
-			for (int i = 0; i < length; i++)
-				result[i] += a1[i] / (a2[i] * a3[i]);
-		}
-
-		DllExport void CalcDivMultMinus(int length, complex<double>* a1, complex<double>* a2, complex<double>* a3, complex<double>* result)
-		{
-#pragma simd
-			for (int i = 0; i < length; i++)
-				result[i] -= a1[i] / (a2[i] * a3[i]);
-		}
-
-		DllExport void CalcDivPlus(int length, complex<double>* a1, complex<double>* a2, complex<double>* result)
-		{
-#pragma simd
-			for (int i = 0; i < length; i++)
-				result[i] += a1[i] / a2[i];
-		}
-
-		DllExport void CalcDivMinus(int length, complex<double>* a1, complex<double>* a2, complex<double>* result)
-		{
-#pragma simd
-			for (int i = 0; i < length; i++)
-				result[i] -= a1[i] / a2[i];
-		}
-
+	
 		DllExport void CalcExp(int length, complex<double>* eta, double factor, complex<double>* result)
 		{
 #pragma simd
 			for (int i = 0; i < length; i++)
 				result[i] = exp(eta[i] * factor);
 		}
-
-		DllExport void CalcDoubleExp(int length, complex<double>* eta1, double factor1, complex<double>* eta2, double factor2, complex<double>* result)
-		{
-#pragma simd
-			for (int i = 0; i < length; i++)
-				result[i] = exp(eta1[i] * factor1 + eta2[i] * factor2);
-		}
-
 
 		DllExport complex16 CalcDotProduct(double* hank, complex16* fk, int length, int shift)
 		{
@@ -116,7 +67,7 @@ namespace Native
 			return s;
 		}
 
-		void ClearBuffer(complex16* buffer, int length)
+		DllExport void ClearBuffer(complex16* buffer, int length)
 		{
 			memset(buffer, 0, length * 16);
 		}
