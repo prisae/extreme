c961285298fbc6f8407969f2a3a132a30571389e
Alexey Geraskin <alexey.geraskin@gmail.com>
2015-10-03 20:32:29 +0200

    distributed solving is working, now doing observations

--- giem2g_mult.cpp
+++ giem2g_mult.cpp
@@ -1,6 +1,7 @@
 #include <mkl_service.h>
 #include <mkl.h>
 
+#include <complex>
 #include <iostream>
 
 #ifdef WINDOWS
@@ -9,9 +10,11 @@
 #define DllExport 
 #endif
 
+
+
 using namespace std;
 
-typedef struct{ double re; double im; } complex16;
+typedef struct { double re; double im; } complex16;
 
 namespace Native
 {
@@ -19,12 +22,12 @@ namespace Native
 	{
 		DllExport void ZgemvAsymTrans(int nz, complex16* alpha, complex16* beta, void* green, void* input, void* result)
 		{
-			cblas_zgemv(CblasColMajor, CblasTrans, nz, nz, alpha, green, nz, input, 1, beta, result, 1);
+			cblas_zgemv(CblasRowMajor, CblasTrans, nz, nz, alpha, green, nz, input, 1, beta, result, 1);
 		}
 
 		DllExport void ZgemvAsymNoTrans(int nz, complex16* alpha, complex16* beta, void* green, void* input, void* result)
 		{
-			cblas_zgemv(CblasColMajor, CblasNoTrans, nz, nz, alpha, green, nz, input, 1, beta, result, 1);
+			cblas_zgemv(CblasRowMajor, CblasNoTrans, nz, nz, alpha, green, nz, input, 1, beta, result, 1);
 		}
 
 		DllExport void ZgemvSym(int nz, MKL_Complex16* alpha, MKL_Complex16* beta, MKL_Complex16* green, MKL_Complex16* input, MKL_Complex16* result)
