4c6d115df4ed68af9c5bbcf4df8ce281a447ce0a
Alexey Geraskin <alexey.geraskin@gmail.com>
2015-10-05 20:36:28 +0200

    AtoO mult is working on

--- giem2g_mult.cpp
+++ giem2g_mult.cpp
@@ -30,6 +30,11 @@ namespace Native
 			cblas_zgemv(CblasRowMajor, CblasNoTrans, nz, nz, alpha, green, nz, input, 1, beta, result, 1);
 		}
 
+		DllExport void ZgemvAtoO(int n, int m, complex16* alpha, complex16* beta, void* green, void* input, void* result)
+		{
+			cblas_zgemv(CblasRowMajor, CblasTrans, n, m, alpha, green, m, input, 1, beta, result, 1);
+		}
+
 		DllExport void ZgemvSym(int nz, MKL_Complex16* alpha, MKL_Complex16* beta, MKL_Complex16* green, MKL_Complex16* input, MKL_Complex16* result)
 		{
 			const char u = 'U';
