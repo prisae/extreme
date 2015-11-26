eb3795f3f9c5c62c8dc907d82e0847e3148d113a
Alexey Geraskin <alexey.geraskin@gmail.com>
2015-11-26 16:56:59 +0100

    Misha: make it hard, baby

--- StrictThreadPool.cs
+++ StrictThreadPool.cs
@@ -66,7 +66,7 @@ namespace Extreme.Core
             //        _threads.AddLast(Thread.CurrentThread);
             //    }
             //    task = null;
-            }
+            //}
         }
 
 
