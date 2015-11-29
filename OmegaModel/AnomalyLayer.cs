f5fb68b9167a9f263c4872c46381386f34ae2439
Alexey Geraskin <alexey.geraskin@gmail.com>
2015-11-29 19:13:04 +0100

    anomaly sigma in one 3D array

---
+++ OmegaModel/AnomalyLayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Linq;
+
+namespace Extreme.Core
+{
+    public class AnomalyLayer : IAnomalyLayer
+    {
+        public AnomalyLayer(decimal depth, decimal thickness)
+        {
+            Depth = depth;
+            Thickness = thickness;
+        }
+
+        public decimal Depth { get; }
+        public decimal Thickness { get; }
+    }
+}
--- OmegaModel/IAnomalyLayer.cs
+++ OmegaModel/IAnomalyLayer.cs
@@ -6,14 +6,6 @@ namespace Extreme.Core
 {
     public interface IAnomalyLayer
     {
-        /// <summary>
-        /// Provides zeta value at specified index
-        /// </summary>
-        /// <param name="i"></param>
-        /// <param name="j"></param>
-        /// <returns></returns>
-        Complex GetZeta(int i, int j);
-
         decimal Depth { get; }
         decimal Thickness { get; }
     }
