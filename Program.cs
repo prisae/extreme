d3347fb4b2bfca8952f65e830e33dce41e505d22
Alexey Geraskin <alexey.geraskin@gmail.com>
2015-11-29 16:17:52 +0100

    Logging refactoring

--- Program.cs
+++ Program.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using Microsoft.Office.Interop.Excel;
-using ModelCreaters;
-using Porvem.Cartesian;
-using Porvem.Cartesian.Magnetotellurics;
-using Extreme.Cartesian.Model;
-using Extreme.Cartesian.Project;
-using Extreme.Core;
-
-namespace Profiling
+﻿namespace Profiling
 {
     public static class Program
     {
