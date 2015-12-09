d699192b3051c8f0396dc34104a80d2993fc1e68
Alexey Geraskin <alexey.geraskin@gmail.com>
2015-12-09 20:52:37 +0100

    add mask

--- Magnetotellurics/ImpedanceTensor.cs
+++ Magnetotellurics/ImpedanceTensor.cs
@@ -4,6 +4,8 @@ namespace Porvem.Magnetotellurics
 {
     public class ImpedanceTensor : Tensor
     {
+        public static ImpedanceTensor Empty = new ImpedanceTensor(0, 0, 0, 0);
+
         public ImpedanceTensor(Complex xx, Complex xy, Complex yx, Complex yy)
             : base (xx, xy, yx, yy)
         {
--- Magnetotellurics/MagneticTipper.cs
+++ Magnetotellurics/MagneticTipper.cs
@@ -4,6 +4,8 @@ namespace Porvem.Magnetotellurics
 {
     public class MagneticTipper : Tipper
     {
+        public static MagneticTipper Empty = new MagneticTipper(0, 0);
+
         public MagneticTipper(Complex zx, Complex zy)
             : base(zx, zy)
         { }
