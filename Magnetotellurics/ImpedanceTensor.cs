54d2be9ddb58a1002ed03360ae3b56f14858dde7
Alexey Geraskin <alexey.geraskin@gmail.com>
2015-12-10 19:51:24 +0100

    workign vertion with avdeev mask example

--- Magnetotellurics/ImpedanceTensor.cs
+++ Magnetotellurics/ImpedanceTensor.cs
@@ -5,6 +5,7 @@ namespace Porvem.Magnetotellurics
     public class ImpedanceTensor : Tensor
     {
         public static ImpedanceTensor Empty = new ImpedanceTensor(0, 0, 0, 0);
+        public static ImpedanceTensor EqualOne = new ImpedanceTensor(1, 1, 1, 1);
 
         public ImpedanceTensor(Complex xx, Complex xy, Complex yx, Complex yy)
             : base (xx, xy, yx, yy)
--- Magnetotellurics/MagneticTipper.cs
+++ Magnetotellurics/MagneticTipper.cs
@@ -5,6 +5,7 @@ namespace Porvem.Magnetotellurics
     public class MagneticTipper : Tipper
     {
         public static MagneticTipper Empty = new MagneticTipper(0, 0);
+        public static MagneticTipper EqualOne = new MagneticTipper(1, 1);
 
         public MagneticTipper(Complex zx, Complex zy)
             : base(zx, zy)
