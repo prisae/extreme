1109991e7f69460f11edd96e8075a6b4c19adaa8
Mikhail Kruglyakov <m.kruglyakov@gmail.com>
2016-03-01 00:48:37 +0300

    bug fix in convertion sigma to zeta

--- Model/Omega/OmegaModelUtils.cs
+++ Model/Omega/OmegaModelUtils.cs
@@ -9,11 +9,14 @@ namespace Extreme.Cartesian.Model
     public static class OmegaModelUtils
     {
         public const double Mu0 = (4.0 * Math.PI * 1.0E-07);
-        public const double Epsilon0 = (1E-9 / (36 * Math.PI));
+		public const double Epsilon0 = 1E0/(Mu0*299792458L*299792458L);	//(1E-9 / (36 * Math.PI));
+
+
 
         public static Complex ConvertSigmaToZeta(double omega, Complex sigma)
         {
-            return new Complex((sigma.Real + omega * Epsilon0), (sigma.Imaginary - omega * Epsilon0));
+          //  return new Complex((sigma.Real + omega * Epsilon0), (sigma.Imaginary - omega * Epsilon0));
+			return new Complex((sigma.Real), (sigma.Imaginary - omega * Epsilon0));
         }
 
 
@@ -35,7 +38,8 @@ namespace Extreme.Cartesian.Model
         }
 
         private static Complex ToZeta(double sigma, double omega)
-            => new Complex((sigma + omega * Epsilon0), -omega * Epsilon0);
+		=> new Complex(sigma, -omega * Epsilon0);
+            //=> new Complex((sigma + omega * Epsilon0), -omega * Epsilon0);
 
         public static Complex[] GetAllSection1DZeta(this ISection1D<IsotropyLayer> section1D)
         {
