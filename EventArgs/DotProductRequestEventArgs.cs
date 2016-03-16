599e1f8d1ded3a88d772a1c6acf5e6548f1d44d5
Mikhail Kruglyakov <m.kruglyakov@gmail.com>
2016-03-16 14:25:00 +0300

    some optimization in giem2g. change some int to long in extreme

--- EventArgs/DotProductRequestEventArgs.cs
+++ EventArgs/DotProductRequestEventArgs.cs
@@ -4,14 +4,14 @@ namespace Extreme.Fgmres
 {
     public unsafe class DotProductRequestEventArgs : System.EventArgs
     {
-        public int Length { get; }
+		public long Length { get; }
         public int NumberOfDotProducts { get; }
         public Complex* X { get; }
         public Complex* Y { get; }
         public Complex* Result { get; }
 
 
-        public DotProductRequestEventArgs(int length, Complex* x, Complex* y, Complex* result, int numberOfDotProducts)
+        public DotProductRequestEventArgs(long length, Complex* x, Complex* y, Complex* result, int numberOfDotProducts)
         {
             Length = length;
             X = x;
